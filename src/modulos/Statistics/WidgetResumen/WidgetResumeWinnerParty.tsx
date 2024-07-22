4798a54b01e6e5279b7089fecfdc7193c17299da
mario Apple <makroz@hotmail.com>
2024-07-22 17:23:27 -0400

    Refactorizar componente WidgetResumeWinnerParty

--- src/modulos/Statistics/WidgetResumen/WidgetResumeWinnerParty.tsx
+++ src/modulos/Statistics/WidgetResumen/WidgetResumeWinnerParty.tsx
@@ -6,14 +6,19 @@ import DonutChart from "@/mk/components/DonutChart/DonutChart";
 import { getUrlImages } from "@/mk/utils/string";
 import { formatNumber } from "@/mk/utils/numbers";
 
-const WidgetResumeWinnerParty = ({ data, title, subtitle, style, total, onClick = null }: any) => {
- console.log(data, "data desde winnerparty");
+const WidgetResumeWinnerParty = ({
+  data,
+  title,
+  subtitle,
+  style,
+  total,
+  onClick = null,
+}: any) => {
+  console.log(data, "data desde winnerparty");
   const calculatePercentage: any = (totalVotes: number, votes: number) => {
     if (!totalVotes) return 0;
     return ((votes / totalVotes) * 100).toFixed(2);
   };
-
-
 
   return (
     <Card className={styles["widgetResumeWinner"]}>
@@ -22,25 +27,44 @@ const WidgetResumeWinnerParty = ({ data, title, subtitle, style, total, onClick 
           <div>{title}</div>
           <div>{subtitle}</div>
         </div>
-        { onClick && <div onClick={onClick}>Ver más</div>}
+        {onClick && <div onClick={onClick}>Ver más</div>}
       </section>
       <section style={style}>
         {data?.map((item: any, i: number) => (
-          <div
-            key={i}>
+          <div key={i}>
             <div>
-              <Avatar src={getUrlImages("/PAR-" + item?.id + ".png?d=" + item?.updated_at)}
-                name={item?.name} />
+              <Avatar
+                src={getUrlImages(
+                  "/PAR-" + item?.id + ".png?d=" + item?.updated_at
+                )}
+                name={item?.name}
+              />
               <span>{item?.name}</span>
-              {data?.length === 0 ? <span>No hay votos</span> : <span>{formatNumber(item?.total_votos,0)} votos obtenidos</span>}
-
+              {data?.length === 0 ? (
+                <span>No hay votos</span>
+              ) : (
+                <span>
+                  {formatNumber(item?.total_votos, 0)} votos obtenidos
+                </span>
+              )}
             </div>
-            <div style={{ width: "100%", display: "flex", justifyContent: "center" }}>
-              <DonutChart percentage={parseFloat(calculatePercentage(total, Number(item?.total_votos)))} color={item?.color} size={100} />
+            <div
+              style={{
+                width: "100%",
+                display: "flex",
+                justifyContent: "center",
+              }}
+            >
+              <DonutChart
+                percentage={parseFloat(
+                  calculatePercentage(total, Number(item?.total_votos))
+                )}
+                color={item?.color}
+                size={100}
+              />
             </div>
           </div>
         ))}
-
       </section>
     </Card>
   );
