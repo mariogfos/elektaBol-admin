55f64d2b063e814350e45435b1d1ed0882b7ee5a
cristian <crisblanco.fos@gmail.com>
2024-10-09 14:48:12 -0400

    Refactorizar componente de estadísticas y estilos de los widgets y componentes

--- src/components/Users/type.tsx
+++ src/components/Users/type.tsx
@@ -1,16 +1,33 @@
 export const lLevels = [
   "FOS",
   "Partido",
+  "Departamento",
+  "Macroregion", //N
   "Provincia",
-  "Cantón",
-  "Parroquia",
+  "Municipios",
+  "Distritos municipales", //N
+  "localidades",
+  "Unidad vecinal", //N
   "Barrio",
+  //   2:dpto
+  // 3:macroregion
+  // 4:prov
+  // 5:mun (municipios)
+  // 6:dmun (distritos municipales)
+  // 7:local (localidades)
+  // 8:uv (unidad vecinal)
+  // 9: barrio
 ];
 
 export const lEntity = [
   "client_id",
+  "party_id",
+  "dpto_id",
+  "macroregion_id",
   "prov_id",
-  "canton_id",
-  "parish_id",
+  "mun_id",
+  "dmun_id",
+  "local_id",
+  "uv_id",
   "barrio_id",
 ];
