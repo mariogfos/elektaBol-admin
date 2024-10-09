f4d245df25223032e80576704cc441231e2b79f4
DamianMorganti14 <144267354+DamianMorganti14@users.noreply.github.com>
2024-10-09 16:23:07 -0400

    Refactorizar componente de estadísticas y estilos de widgets y componentes

--- src/components/AffiliatesOld/DetailAffiliate.tsx
+++ src/components/AffiliatesOld/DetailAffiliate.tsx
@@ -2,7 +2,7 @@ import { Avatar } from "@/mk/components/ui/Avatar/Avatar";
 import DataModal from "@/mk/components/ui/DataModal/DataModal";
 import { getFullName, getUrlImages } from "@/mk/utils/string";
 import React from "react";
-import styles from "./affiliates.module.css";
+import styles from "./DetailAffiliates.module.css";
 
 type PropsDetailUsers = {
   open: boolean;
