291283be0307675ded137524554c202df34e4c89
sebastian <sebastian.condaty@gmail.com>
2024-12-11 13:34:05 -0400

    Refactor code to improve performance and readability

--- src/modulos/Statistics/WidgetMapa/WidgetMapa.tsx
+++ src/modulos/Statistics/WidgetMapa/WidgetMapa.tsx
@@ -110,229 +110,236 @@ const WidgetMapa = ({
   console.log("isProv", isProv);
 
   if ((param?.level || 0) == 0) path = pathsPais;
-  if (param?.level == 1 && isProv) {
-    path = pathsSantaCruzProvincias;
-  } else {
-    switch (param?.code) {
-      case "9":
-        path = pathsPando;
-        break;
-      case "8":
-        path = pathsBeni;
-        break;
-      case "2":
-        path = pathsLaPaz;
-        break;
-      case "1":
-        path = pathsChuquisaca;
-        break;
-      case "3":
-        path = pathsCochabamba;
-        break;
-      case "4":
-        path = pathsOruro;
-        break;
-      case "5":
-        path = pathsPotosi;
-        break;
-      case "6":
-        path = pathsTarija;
-        break;
-      case "7":
-        path = pathsSantaCruz;
-        break;
-      default:
-        path = pathsPais;
-    }
-  }
 
-  if (param?.level == 2) {
-    switch (param?.code) {
-      case "40":
-        path = pathsC40;
-        break;
-      case "41":
-        path = pathsC41;
-        break;
-      case "42":
-        path = pathsC42;
-        break;
-      case "43":
-        path = pathsC43;
-        break;
-      case "44":
-        path = pathsC44;
-        break;
-      case "45":
-        path = pathsC45;
-        break;
-      case "46":
-        path = pathsC46;
-        break;
-      case "47":
-        path = pathsC47;
-        break;
-      case "48":
-        path = pathsC48;
-        break;
-      case "49":
-        path = pathsC49;
-        break;
-      case "50":
-        path = pathsC50;
-        break;
-      case "51":
-        path = pathsC51;
-        break;
-      case "52":
-        path = pathsC52;
-        break;
-      case "53":
-        path = pathsC53;
-        break;
-      case "54":
-        path = pathsC54;
-        break;
-      case "55":
-        path = pathsC55;
-        break;
-      case "56":
-        path = pathsC56;
-        break;
-      case "57":
-        path = pathsC57;
-        break;
-      case "58":
-        path = pathsC58;
-        break;
-      case "59":
-        path = pathsC59;
-        break;
-      case "60":
-        path = pathsC60;
-        break;
-      case "61":
-        path = pathsC61;
-        break;
-      case "62":
-        path = pathsC62;
-        break;
-      case "63":
-        path = pathsC63;
-        break;
-      case "27":
-        path = pathsC27;
-        break;
-      case "28":
-        path = pathsC28;
-        break;
-      case "26":
-        path = pathsC26;
-        break;
-      case "22":
-        path = pathsC22;
-        break;
-      case "20":
-        path = pathsC20;
-        break;
-      case "23":
-        path = pathsC23;
-        break;
-      case "24":
-        path = pathsC24;
-        break;
-      case "25":
-        path = pathsC25;
-        break;
-      case "6":
-        path = pathsC6;
-        break;
-      case "7":
-        path = pathsC7;
-        break;
-      case "8":
-        path = pathsC8;
-        break;
-      case "9":
-        path = pathsC9;
-        break;
-      case "10":
-        path = pathsC10;
-        break;
-      case "11":
-        path = pathsC11;
-        break;
-      case "12":
-        path = pathsC12;
-        break;
-      case "13":
-        path = pathsC13;
-        break;
-      case "14":
-        path = pathsC14;
-        break;
-      case "15":
-        path = pathsC15;
-        break;
-      case "16":
-        path = pathsC16;
-        break;
-      case "17":
-        path = pathsC17;
-        break;
-      case "18":
-        path = pathsC18;
-        break;
-      case "19":
-        path = pathsC19;
-        break;
-      case "32":
-        path = pathsC32;
-        break;
-      case "31":
-        path = pathsC31;
-        break;
-      case "30":
-        path = pathsC30;
-        break;
-      case "29":
-        path = pathsC29;
-        break;
-      case "36":
-        path = pathsC36;
-        break;
-      case "37":
-        path = pathsC37;
-        break;
-      case "35":
-        path = pathsC35;
-        break;
-      case "34":
-        path = pathsC34;
-        break;
-      case "39":
-        path = pathsC39;
-        break;
-      case "38":
-        path = pathsC38;
-        break;
-      case "2":
-        path = pathsC2;
-        break;
-      case "1":
-        path = pathsC1;
-        break;
-      case "3":
-        path = pathsC3;
-        break;
-      case "4":
-        path = pathsC4;
-        break;
-      case "5":
-        path = pathsC5;
-        break;
-      default:
-        return null;
+  if (isProv) {
+    if (param?.level == 1) {
+      path = pathsSantaCruzProvincias;
+    }
+    if (param?.level == 2) {
+    }
+  } else {
+    if (param?.level == 1) {
+      switch (param?.code) {
+        case "9":
+          path = pathsPando;
+          break;
+        case "8":
+          path = pathsBeni;
+          break;
+        case "2":
+          path = pathsLaPaz;
+          break;
+        case "1":
+          path = pathsChuquisaca;
+          break;
+        case "3":
+          path = pathsCochabamba;
+          break;
+        case "4":
+          path = pathsOruro;
+          break;
+        case "5":
+          path = pathsPotosi;
+          break;
+        case "6":
+          path = pathsTarija;
+          break;
+        case "7":
+          path = pathsSantaCruz;
+          break;
+        default:
+          path = pathsPais;
+      }
+    }
+
+    if (param?.level == 2) {
+      switch (param?.code) {
+        case "40":
+          path = pathsC40;
+          break;
+        case "41":
+          path = pathsC41;
+          break;
+        case "42":
+          path = pathsC42;
+          break;
+        case "43":
+          path = pathsC43;
+          break;
+        case "44":
+          path = pathsC44;
+          break;
+        case "45":
+          path = pathsC45;
+          break;
+        case "46":
+          path = pathsC46;
+          break;
+        case "47":
+          path = pathsC47;
+          break;
+        case "48":
+          path = pathsC48;
+          break;
+        case "49":
+          path = pathsC49;
+          break;
+        case "50":
+          path = pathsC50;
+          break;
+        case "51":
+          path = pathsC51;
+          break;
+        case "52":
+          path = pathsC52;
+          break;
+        case "53":
+          path = pathsC53;
+          break;
+        case "54":
+          path = pathsC54;
+          break;
+        case "55":
+          path = pathsC55;
+          break;
+        case "56":
+          path = pathsC56;
+          break;
+        case "57":
+          path = pathsC57;
+          break;
+        case "58":
+          path = pathsC58;
+          break;
+        case "59":
+          path = pathsC59;
+          break;
+        case "60":
+          path = pathsC60;
+          break;
+        case "61":
+          path = pathsC61;
+          break;
+        case "62":
+          path = pathsC62;
+          break;
+        case "63":
+          path = pathsC63;
+          break;
+        case "27":
+          path = pathsC27;
+          break;
+        case "28":
+          path = pathsC28;
+          break;
+        case "26":
+          path = pathsC26;
+          break;
+        case "22":
+          path = pathsC22;
+          break;
+        case "20":
+          path = pathsC20;
+          break;
+        case "23":
+          path = pathsC23;
+          break;
+        case "24":
+          path = pathsC24;
+          break;
+        case "25":
+          path = pathsC25;
+          break;
+        case "6":
+          path = pathsC6;
+          break;
+        case "7":
+          path = pathsC7;
+          break;
+        case "8":
+          path = pathsC8;
+          break;
+        case "9":
+          path = pathsC9;
+          break;
+        case "10":
+          path = pathsC10;
+          break;
+        case "11":
+          path = pathsC11;
+          break;
+        case "12":
+          path = pathsC12;
+          break;
+        case "13":
+          path = pathsC13;
+          break;
+        case "14":
+          path = pathsC14;
+          break;
+        case "15":
+          path = pathsC15;
+          break;
+        case "16":
+          path = pathsC16;
+          break;
+        case "17":
+          path = pathsC17;
+          break;
+        case "18":
+          path = pathsC18;
+          break;
+        case "19":
+          path = pathsC19;
+          break;
+        case "32":
+          path = pathsC32;
+          break;
+        case "31":
+          path = pathsC31;
+          break;
+        case "30":
+          path = pathsC30;
+          break;
+        case "29":
+          path = pathsC29;
+          break;
+        case "36":
+          path = pathsC36;
+          break;
+        case "37":
+          path = pathsC37;
+          break;
+        case "35":
+          path = pathsC35;
+          break;
+        case "34":
+          path = pathsC34;
+          break;
+        case "39":
+          path = pathsC39;
+          break;
+        case "38":
+          path = pathsC38;
+          break;
+        case "2":
+          path = pathsC2;
+          break;
+        case "1":
+          path = pathsC1;
+          break;
+        case "3":
+          path = pathsC3;
+          break;
+        case "4":
+          path = pathsC4;
+          break;
+        case "5":
+          path = pathsC5;
+          break;
+        default:
+          return null;
+      }
     }
   }
 
