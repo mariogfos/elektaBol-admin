1faf22220b0d9af389a193aa130a868e7b2ece52
DamianMorganti14 <144267354+DamianMorganti14@users.noreply.github.com>
2024-12-17 09:34:29 -0400

    added className and style in tooltip

--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -4,11 +4,13 @@ type PropsType = {
   title: string;
   children: any;
   position?: "top" | "bottom" | "left" | "right";
+  style?: any;
+  className?: string;
 };
 
-const Tooltip = ({ title, children, position = "left" }: PropsType) => {
+const Tooltip = ({ title, children, position = "left",style,className }: PropsType) => {
   return (
-    <div className={styles.container}>
+    <div className={`${styles.container}  ${className}`} style={style}>
       <span className={`${styles.tooltip} ${styles[position]}`}>{title}</span>
       {children}
     </div>
