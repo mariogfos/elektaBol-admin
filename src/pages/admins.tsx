87f5f5428e6129d94efd78b0dfb3422e9448d014
DamianMorganti14 <144267354+DamianMorganti14@users.noreply.github.com>
2024-12-19 09:34:06 -0400

    added admins module

--- src/pages/admins.tsx
+++ src/pages/admins.tsx
@@ -1,8 +1,9 @@
 import Users from "@/components/Users/Users";
+import Admins from "@/modulos/Admins/Admins";
 
 
 const affiliatesPage = () => {
-  return <Users />;
+  return <Admins />;
 };
 
 export default affiliatesPage;
